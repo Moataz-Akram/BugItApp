33094f3edbfaab397e8ce329fc6e5010c6847f3a
moataz-akram <moatazakram0@gmail.com>
2024-09-13 16:38:34 +0300

    Initial Commit

---
+++ BugItApp/BugItAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugItAppApp.swift
+//  BugItApp
+//
+//  Created by Moataz Akram on 13/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BugItAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
